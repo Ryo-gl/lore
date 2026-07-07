cc550451922a20192e87eba8f4acb9cdbd744408
Ryo-gl <endt8085@gmail.com>
2026-07-07 23:32:08 +0300

    script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const button = document.getElementById('clickMe');
-    const message = document.getElementById('message');
-
-    button.addEventListener('click', function() {
-        message.textContent = 'Спасибо, что нажали! 🎉';
-        message.style.color = '#007bff';
-    });
-});
